a913987ce14c0c6981ee92bef35d3440c5bf7029
lyraxvincent <njongevincent@gmail.com>
2020-09-24 20:11:51 +0300

    remove unwanted files

--- build/lib/DataStand/__init__.py
+++
@@ -1,2 +0,0 @@
-from DataStand import DataStand
-from DataStand import impute_missing
--- build/lib/DataStand/test.py
+++
@@ -1,9 +0,0 @@
-from DataStand import DataStand
-from DataStand import impute_missing
-import pandas as pd
-
-train = pd.read_csv('train.csv')
-
-#DataStand(train)
-impute_missing(train)
-
