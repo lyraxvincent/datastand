636b0b99d29ae3caa422a20b8921f9ce70d47396
lyraxvincent <njongevincent@gmail.com>
2020-09-30 22:08:31 +0300

    improve package description

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ setuptools.setup(
     license='MIT  ',
     author="Vincent Njonge",
     author_email="njongevincent@gmail.com",
-    description="A python package that helps Data Scientists, Machine Learning Engineers and Analysts to quickly explore and understand a dataset.",
+    description="""A python package to help users especially Data Scientists, Machine Learning Engineers and 
+    Analysts to better understand a dataset. Gives quick insights about given data; general dataset statistics, 
+    size and shape of dataset, number of numerical and non-numerical columns, small overview of dataset, missing 
+    data statistics, missing data heatmap and provides methodology to impute missing data.""",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lyraxvincent/DataStand",
