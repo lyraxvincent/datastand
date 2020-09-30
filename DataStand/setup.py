16d4b76bdc3110627b8f3224c7dddf7e142e49b1
lyraxvincent <njongevincent@gmail.com>
2020-09-30 16:59:55 +0300

    publish V1.2

--- DataStand/setup.py
+++ DataStand/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
      name='DataStand',
-     version='1.1',
+     version='1.2',
      scripts=['DataStand'],
      author="Vincent Njonge",
      author_email="njongevincent@gmail.com",
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 setuptools.setup(
     name='DataStand',
     packages=['DataStand'],
-    version='1.1',
+    version='1.2',
     license='MIT  ',
     author="Vincent Njonge",
     author_email="njongevincent@gmail.com",
