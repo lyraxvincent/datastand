79eedcd2613ff57cc0d34f594e131e51e2aad304
lyraxvincent <njongevincent@gmail.com>
2022-01-25 11:56:15 +0300

    publish version 2.0

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open("README.md", "r") as f:
 setuptools.setup(
     name='datastand',
     packages=['datastand'],
-    version='1.9',
+    version='2.0',
     license='MIT  ',
     author="Vincent Njonge",
     author_email="njongevincent@gmail.com",
-    description="""A python package to help users especially Data Scientists, Machine Learning Engineers and 
-    Analysts to better understand a dataset. Gives quick insights about the given dataset; general dataset statistics, 
-    size and shape of dataset, number of unique data types, number of numerical and non-numerical columns, small 
-    overview of dataset, data description, missing data statistics, missing data heatmap and provides methodology 
-    to impute missing data.""",
+    description="""A python package to help Data Scientists, Machine Learning Engineers and Analysts 
+    better understand data. Gives quick insights about given data; general dataset statistics, size and 
+    shape of dataset, number of unique data types, number of numerical and non-numerical columns, small 
+    overview of dataset, missing data statistics, missing data heatmap and provides methodology to impute 
+    missing data.""",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lyraxvincent/datastand",
