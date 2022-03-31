cce51d820c683838c0982a270a8cd19cc04fd007
lyraxvincent <njongevincent@gmail.com>
2022-03-31 15:38:12 +0300

    publish version 2.4.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 setuptools.setup(
     name='datastand',
     packages=['datastand'],
-    version='2.4.1',
+    version='2.4.2',
     license='MIT',
     author="Vincent Njonge",
     author_email="njongevincent@gmail.com",
